0dc78f1c722bb57f8f3fcc08dad6a5e8f000284e
jdominguez <76186137+jfull360@users.noreply.github.com>
2023-08-18 14:15:00 -0600

    FIRST BETA OF OracleHotelAlura PROJECT
    :+1:  ADD INTERFACES WITH IMAGES
    :+1:  ADD VALIDATIONS TO SOME SWING CONTROLS
    :+1:  ADD JAR  TO CALENDAR AND SQL

---
+++ src/com/oracle/utils.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.oracle;
+
+import java.awt.Image;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author JORGE DOMINGUEZ
+ */
+public class utils {
+
+    utils() {}
+
+    protected void SetImage(JLabel labelName, String image) {
+        ImageIcon imageicon = new ImageIcon(image);
+        Icon icon = new ImageIcon(imageicon.getImage().getScaledInstance(labelName.getWidth(), labelName.getHeight(), Image.SCALE_DEFAULT));
+        labelName.setIcon(icon);
+        //this.repaint();
+    }
+
+    public static boolean isNumeric(String cadena) {
+        boolean resultado;
+        try {
+            Double.parseDouble(cadena);
+            resultado = true;
+        } catch (Exception excepcion) {
+            resultado = false;
+        }
+        return resultado;
+    }
+
+}
